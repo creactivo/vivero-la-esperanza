0c79a472645ce32f2353d4907d07773829b027d2
Creactivo <zonacreactiva@gmail.com>
2026-06-22 13:58:46 -0500

    arreglos de despliegue para vercel, ojo que hizo cambios en pnpm-lockyaml

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
-// output: 'static' → SSG (Static Site Generation).
-// El build requiere que el backend esté corriendo para compilar.
-// Las páginas se generan de forma estática en tiempo de build.
 export default defineConfig({
   integrations: [react()],
-  output: 'static',
+  output: 'server',
+  adapter: vercel(),
   server: {
     port: 4321,
     host: true
